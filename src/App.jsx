4b372949f85d7eb1661f87f9bb160cf4cbd5ae2f
Prakhyath19 <prakhyathteddy007@gmail.com>
2025-06-23 11:27:04 +0530

    Made the website more responsive

--- src/App.jsx
+++ src/App.jsx
@@ -4,7 +4,7 @@ import { useEffect } from "react";
 function Square({ value, onSquareClick }) {
   return (
     <button
-      className="w-8 h-8  flex items-center justify-center border p-8 text-xl  
+      className="w-10 h-10 md:w-18 md:h-18  flex items-center justify-center border p-4 text-md md:text-xl  
       "
       onClick={onSquareClick}
     >
@@ -64,7 +64,7 @@ function Board({ xIsNext, squares, onPlay, xQ, oQ, resetBoard }) {
     // >
     <div
       id="card"
-      className="p-8 px-5 bg-black/30
+      className="px-4 py-4 md:py-8  md:px-5 bg-black/30
        text-white rounded-xl shadow-lg  shadow-black
         flex flex-col items-center justify-center"
     >
@@ -72,8 +72,10 @@ function Board({ xIsNext, squares, onPlay, xQ, oQ, resetBoard }) {
         Tic-Tac-Toe-2
       </h1>
       <div>
-        <h2 className="text-3xl underline font-mono text-slate-500">Rules</h2>
-        <ol className="text-lg text-slate-400">
+        <h2 className="text-xl md:text-3xl underline font-mono text-slate-500">
+          Rules
+        </h2>
+        <ol className="text-md md:text-lg text-slate-400">
           <li>1. Only three X's or O's are allowed at a time on the board. </li>
           <li>2. The oldest X or O will be removed after 3 chances.</li>
           <li>3. This game will not be a draw.</li>
@@ -81,26 +83,22 @@ function Board({ xIsNext, squares, onPlay, xQ, oQ, resetBoard }) {
       </div>
       <div
         id="status-message"
-        className="mt-2 font-mono text-xl  text-bold underline underline-offset-2 pb-1"
+        className="mt-2 font-mono text-md md:text-xl  text-bold underline underline-offset-2 pb-1"
       >
         {status}
       </div>
-      <div
-        id="board"
-        className=" mt-4
-      "
-      >
+      <div id="board" className="mt-4">
         <div className="flex">
           <Square value={squares[0]} onSquareClick={() => handleClick(0)} />
           <Square value={squares[1]} onSquareClick={() => handleClick(1)} />
           <Square value={squares[2]} onSquareClick={() => handleClick(2)} />
         </div>
-        <div className="flex  ">
+        <div className="flex">
           <Square value={squares[3]} onSquareClick={() => handleClick(3)} />
           <Square value={squares[4]} onSquareClick={() => handleClick(4)} />
           <Square value={squares[5]} onSquareClick={() => handleClick(5)} />
         </div>
-        <div className="flex ">
+        <div className="flex">
           <Square value={squares[6]} onSquareClick={() => handleClick(6)} />
           <Square value={squares[7]} onSquareClick={() => handleClick(7)} />
           <Square value={squares[8]} onSquareClick={() => handleClick(8)} />
@@ -166,8 +164,12 @@ export default function Game() {
   return (
     <>
       <div
-        className=" min-h-screen bg-black/90 bg-gradient-to-br
-          to-blue-950 flex items-center justify-center"
+        className=" min-h-screen min-w-screen
+         bg-black/90 bg-gradient-to-br
+        to-blue-950
+          flex items-center justify-center
+          overflow-hidden
+          "
       >
         <Board
           xIsNext={xIsNext}
